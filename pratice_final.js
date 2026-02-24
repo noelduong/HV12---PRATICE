f7025994fcd6ead86e462724dc3cb3b6514a6d63
noelduong <duyduong.noel@gmail.com>
2026-02-24 20:58:19 +0700

    first commit

--- pratice_final.js
+++ pratice_final.js
@@ -1,6 +1,9 @@
-let time =10 
+let totaltime = 30
+let time = totaltime
 while (time >0) {
-    if (time === 5) {
+    let elapsed = totaltime - time
+    
+    if (elapsed === time) {
         console.log("Đã qua nửa thời gian!")
     } else {
         console.log(`Còn ${time} giây nữa!`)

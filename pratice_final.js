66a4f81ed3bb2572f3cbb0e455b1a60d578f2b51
noelduong <duyduong.noel@gmail.com>
2026-02-25 22:22:06 +0700

    first commit

--- pratice_final.js
+++ pratice_final.js
@@ -20,7 +20,7 @@ for (let i = 1; i <= numOforder; i++) {
 }
 console.log(`Tổng tiền phải trả: ${total.toLocaleString()} VND`)*/
 
-let price = Math.floor(Math.random() * (5_000_000 - 1_00_000 + 1)) + 1_00_000;
+/**let price = Math.floor(Math.random() * (5_000_000 - 1_00_000 + 1)) + 1_00_000;
 let hasDiscount = Math.round(Math.random());
 if (hasDiscount === 1) {
   let discountPercent = Math.floor(Math.random() * (50 - 10 + 1)) + 10;
@@ -32,4 +32,21 @@ if (hasDiscount === 1) {
 } else {
   console.log(`Gia goc: ${price.toLocaleString()} VND`);
   console.log("Không có giảm giá!");
-}
+}*/
+
+/**let n = Math.floor(Math.random() * (10 - 1 + 1)) + 1
+console.log(n)*/
+
+/**let n = Math.floor(Math.random() * (20 - 1 +1)) + 1
+if ( n % 2 === 0) {
+    console.log(`${n} là số chẵn`)
+} else {console.log(`${n} là số lẻ`)
+}*/
+
+/**let n = Math.floor(Math.random() * (1_000_000 - 100_000 + 1)) + 100_000
+let disCount = Math.floor(Math.random() * (30 - 10 + 1)) + 10 
+let finalPrice = n - (n * disCount / 100)
+
+console.log(`Giá gốc: ${n.toLocaleString()} VNĐ`)
+console.log(` Giảm giá: ${disCount}%`)
+console.log(` Giá sau khi giảm: ${finalPrice.toLocaleString()} VNĐ`)*/

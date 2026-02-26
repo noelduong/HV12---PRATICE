f6ca23faab813b06561b07d62a57637907381000
noelduong <duyduong.noel@gmail.com>
2026-02-26 21:25:30 +0700

    first commit

--- pratice_day6.js
+++ pratice_day6.js
@@ -4,6 +4,7 @@
 printName("Thầy Sang", "0199299292");
 printName("Khang", "0123456789");
 printName("Thư", "0987654321");*/
+/**const { useCallback } = require("react");
 
 /**function introduce (name, age, location) {
     console.log(` Xin chào, tôi tên là ${name}, năm nay tôi ${age} tuổi. Tôi đang sinh sống ở ${location}. `)
@@ -16,8 +17,39 @@ return area;
 }
 console.log(calculateArea(2,5));*/
 
-function calcuLatePythagorean(canhGocvuong1, canhGocvuong2) {
+/**function calcuLatePythagorean(canhGocvuong1, canhGocvuong2) {
     let canhHuyen = Math.sqrt(Math.pow(canhGocvuong1,2) + Math.pow(canhGocvuong2,2))
     return canhHuyen;
 } 
-console.log(calcuLatePythagorean(3,4));
+console.log(calcuLatePythagorean(3,4));*/
+
+
+/**const myFunc = function() {
+    console.log("Hello J");
+}
+myFunc();*/
+
+/** Arrow function 
+const myFunc = () => {
+    console.log("Hello J");
+}
+myFunc()*/
+
+/**const myFunc = (a, b) => (a + b);
+console.log(myFunc(4, 5));*/
+
+const caculator = (number1, number2, operator) => {
+    if (operator === "+") {
+        return number1 + number2;
+    } else if (operator === "-") {
+        return number1 - number2;
+    } else if (operator === "*") {
+        return number1 * number2;
+    } else if (operator === "/") {
+        return number1 / number2;
+    } else { 
+        return "Invalid operator";
+    }
+    }
+    console.log(caculator(2, 4, "*"));
+    console.log(caculator(2, 1, "/"))

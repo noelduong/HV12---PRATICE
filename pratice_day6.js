0ca21c12590272fd0f2affc1efc01a06a295f1f3
noelduong <duyduong.noel@gmail.com>
2026-02-26 20:14:42 +0700

    first commit

--- pratice_day6.js
+++ pratice_day6.js
@@ -1,6 +1,11 @@
-function printName(name) {
-  console.log(`Hello ${name}`);
+/**function printName(name, phone) {
+  console.log(`Hello ${name}, Phone number: ${phone}`);
 }
-printName("Thầy Sang");
-printName("Khang");
-printName("Thư");
+printName("Thầy Sang", "0199299292");
+printName("Khang", "0123456789");
+printName("Thư", "0987654321");*/
+
+function introduce (name, age, location) {
+    console.log(` Xin chào, tôi tên là ${name}, năm nay tôi ${age} tuổi. Tôi đang sinh sống ở ${location}. `)
+}
+introduce("Tuyền", 27, "Hồ Chí Minh")

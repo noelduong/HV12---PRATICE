bc4c5998ff05673aff82316d843c49a5c1860a91
noelduong <duyduong.noel@gmail.com>
2026-02-26 20:48:19 +0700

    first commit

--- pratice_day6.js
+++ pratice_day6.js
@@ -5,7 +5,19 @@ printName("Thầy Sang", "0199299292");
 printName("Khang", "0123456789");
 printName("Thư", "0987654321");*/
 
-function introduce (name, age, location) {
+/**function introduce (name, age, location) {
     console.log(` Xin chào, tôi tên là ${name}, năm nay tôi ${age} tuổi. Tôi đang sinh sống ở ${location}. `)
 }
-introduce("Tuyền", 27, "Hồ Chí Minh")
+introduce("Tuyền", 27, "Hồ Chí Minh")*/
+
+/**function calculateArea(length, width) {
+let area = length * width;
+return area;
+}
+console.log(calculateArea(2,5));*/
+
+function calcuLatePythagorean(canhGocvuong1, canhGocvuong2) {
+    let canhHuyen = Math.sqrt(Math.pow(canhGocvuong1,2) + Math.pow(canhGocvuong2,2))
+    return canhHuyen;
+} 
+console.log(calcuLatePythagorean(3,4));

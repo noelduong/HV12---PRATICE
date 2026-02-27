5c6f6d48471c8dbb54e8b9d709f2afe27511928c
noelduong <duyduong.noel@gmail.com>
2026-02-27 22:08:45 +0700

    first commit

--- pratice_day6.js
+++ pratice_day6.js
@@ -23,7 +23,6 @@ console.log(calculateArea(2,5));*/
 } 
 console.log(calcuLatePythagorean(3,4));*/
 
-
 /**const myFunc = function() {
     console.log("Hello J");
 }
@@ -54,19 +53,48 @@ console.log(myFunc(4, 5));*/
     console.log(caculator(2, 4, "*"));
     console.log(caculator(2, 1, "/"))*/
 
-    const caculator = (number1, number2, operator) => {
-        switch (operator) {
-            case "+":
-                return number1 + number2;
-                case "-":
-                    return number1 - number2;
-                    case "*":
-                        return number1 * number2;
-                        case "/":
-                            return number1 / number2;
-                            default:
-                                return "Invalid operator";
+/**const caculator = (number1, number2, operator) => {
+    switch (operator) {
+        case "+":
+        return number1 + number2;
+                
+        case "-":
+        return number1 - number2;
+                    
+        case "*":
+        return number1 * number2;
+                        
+        case "/":
+        return number1 / number2;
+                            
+        default:
+        return "Invalid operator";
         }
     }
     console.log(caculator(2, 4, "*"));
-    console.log(caculator(2, 1, "/"))
+    console.log(caculator(4, 2, "/"))*/
+
+const findDayOfWeek = (day, month, year) => {
+  if (month < 1 || month > 12){
+    return "Invalid month";
+  }
+  let maxday = 31
+  if (month ===4 || month === 6 || month === 9 || month === 11) {
+    maxday = 30
+  }
+  if (month === 2) {
+    maxday = 28
+  }
+  if ( day < 1 || day > maxday) {
+    return "Invalid day";
+  }
+  const d = new Date(year, month -1 , day).getDay();
+  if (d === 0) return "Sunday";
+    if (d === 1) return "Monday";
+    if (d === 2) return "Tuesday";
+    if (d === 3) return "Wednesday";
+    if (d === 4) return "Thursday";
+    if (d === 5) return "Friday";
+    if (d === 6) return "Saturday";
+}
+console.log(findDayOfWeek(4, 8, 2023));

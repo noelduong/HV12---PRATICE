95b4b89f3b83c76d15be80d050f10c6cf4db31be
noelduong <duyduong.noel@gmail.com>
2026-02-28 20:12:58 +0700

    first commit

--- pratice_280226.js
+++ pratice_280226.js
@@ -3,18 +3,7 @@ let fruits = ["Orange", "Apple", "Banana", "Grapes"];
 /**console.log(fruits.length)*/
 /**console.log(fruits[fruits.length -1])*/
 
-const carts = [
-  "Milk",
-  "Shirt",
-  "Pant",
-  "Hat",
-  "Shoes",
-  "Socks",
-  "Watch",
-  "Bag",
-  "Belt",
-  "Glasses",
-];
+const carts = [ "Milk","Shirt","Pant","Hat","Shoes","Socks","Watch","Bag","Belt","Glasses",];
 console.log(` Total items in cart:${carts.length}`);
 console.log(`Last item in cart: ${carts[carts.length - 1]}`);
 console.log(` First item in cart: ${carts[0]}`);

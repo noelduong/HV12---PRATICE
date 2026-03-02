a8dc9b93c5777ee7f20c2968d3f5354be8b130fc
noelduong <duyduong.noel@gmail.com>
2026-03-02 22:17:52 +0700

    first commit

--- pratice_280226.js
+++ pratice_280226.js
@@ -40,15 +40,38 @@ console.log(carts);*/
 /**let box = matrix[1][2]*/
 /**console.log(matrix[2][2]);*/
 
-
-const friends = [ "An", "Jena", "Dương"]
-friends.push("Hùng")
+/**const friends = ["An", "Jena", "Dương"];
+friends.push("Hùng");
 console.log(`Total number of friends ${friends.length}`);
 console.log(`First friend: ${friends[0]}`);
-friends.unshift("Mai")
-friends.pop()
-friends.splice(2, 1, "Trang")
+friends.unshift("Mai");
+friends.pop();
+friends.splice(2, 1, "Trang");
 for (let i = 0; i < friends.length; i++) {
-    console.log(friends[i]);
-}
+  console.log(friends[i]);
+}*/
+/**Bài 1*/
+const countValidCases = (memoryUsages, limit) => {
+  let count = 0;
+  for (let i = 0; i < memoryUsages.length; i++) {
+    if (memoryUsages[i] <= limit) {
+      count++;
+    }
+  }
+  return count;
+};
 
+console.log(countValidCases([32, 45, 28, 50, 64, 20], 40));
+
+const findMaxSpike = (excutionsTimes) => {
+  let maxSpike = 0;
+  for (let i = 1; i < excutionsTimes.length; i++) {
+    const diff = excutionsTimes[i] - excutionsTimes[i - 1];
+    if (diff > maxSpike) {
+      maxSpike = diff;
+    }
+  }
+  return maxSpike;
+};
+console.log(findMaxSpike([15, 30, 18, 40, 25, 50]));
+console.log(findMaxSpike([50, 40, 30, 30, 20]));
